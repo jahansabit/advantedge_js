826966dca17ef79d4d4e240f679470e3a90ba02f
jahansabit <95720320+jahansabit@users.noreply.github.com>
2023-08-04 12:11:34 +0600

    added new survey autofills

--- form_magic.js
+++ form_magic.js
@@ -1,163 +1,253 @@
-    // Function to get URL parameters
-    function getUrlParameters() {
-        var params = {};
-        var url = window.location.href;
-        var paramArray = url.slice(url.indexOf('?') + 1).split('&');
+// Function to get URL parameters
+function getUrlParameters() {
+    var params = {};
+    var url = window.location.href;
+    var paramArray = url.slice(url.indexOf('?') + 1).split('&');
 
-        for (var i = 0; i < paramArray.length; i++) {
-            var param = paramArray[i].split('=');
-            var key = decodeURIComponent(param[0]);
-            var value = decodeURIComponent(param[1]);
-            params[key] = value;
-        }
-
-        return params;
+    for (var i = 0; i < paramArray.length; i++) {
+        var param = paramArray[i].split('=');
+        var key = decodeURIComponent(param[0]);
+        var value = decodeURIComponent(param[1]);
+        params[key] = value;
     }
 
-    function changeValue(input, value) {
-        var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
-            window.HTMLInputElement.prototype,
-            "value"
-        ).set;
-        nativeInputValueSetter.call(input, value);
+    return params;
+}
 
-        var inputEvent = new Event("input", { bubbles: true });
-        input.dispatchEvent(inputEvent);
-    }
+function changeValue(input, value) {
+    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+        window.HTMLInputElement.prototype,
+        "value"
+    ).set;
+    nativeInputValueSetter.call(input, value);
 
-    // Function to populate input fields
-    function populateInputFields() {
-        var params = getUrlParameters();
+    var inputEvent = new Event("input", { bubbles: true });
+    input.dispatchEvent(inputEvent);
+}
 
-        console.log(params);
-        // Get all input elements with placeholders
-        var inputs = document.querySelectorAll('[name]');
+// Function to populate input fields
+function populateInputFields() {
+    var params = getUrlParameters();
 
-        // Loop through each input element
-        var sleep_time = 0;
-        inputs.forEach(function (input) {
-            var placeholder = input.getAttribute('name');
-            var value = params[placeholder];
+    console.log(params);
+    // Get all input elements with placeholders
+    var inputs = document.querySelectorAll('[name]');
 
-            // If a value is found in the URL parameters, set it as the input's value
-            if (value) {
-                sleep_time += 100;
-                setTimeout(function () {
-                    changeValue(input, value.replaceAll("+", " "));
-                }, sleep_time);
-            }
-        });
-    }
+    // Loop through each input element
+    var sleep_time = 0;
+    inputs.forEach(function (input) {
+        var placeholder = input.getAttribute('name');
+        var value = params[placeholder];
 
-    document.addEventListener('DOMContentLoaded', function () {
-        // Call the function to populate input fields on page load
-        setTimeout(populateInputFields, 1000);
-        // populateInputFields();
-
-        // Retrieve the 'editors_name' value from localStorage
-        var editorsName = localStorage.getItem('editors_name');
-        var editorsNameInput = document.querySelector('[name="editors_name"]');
-
-        // Check if the value exists in localStorage
-        if (editorsName) {
-            if (editorsNameInput) {
-                // Autofill input fields with the 'editors_name' attribute value
-                // editorsNameInput.value = editorsName;
-                setTimeout(function () {
-                    changeValue(editorsNameInput, editorsName);
-                }, 1000);
-
-                // Add an input event listener to the input field
-                editorsNameInput.addEventListener('input', function () {
-                    // Get the updated value from the input field
-                    const newValue = editorsNameInput.value;
-
-                    // Save the updated value to localStorage
-                    localStorage.setItem('editors_name', newValue);
-
-                    // Optionally, you can also display a confirmation message
-                    console.log('Data saved to localStorage:', newValue);
-                });
-            }
-        } else {
-            // Handle the case where 'editors_name' is not found in localStorage
-            console.log('Editor\'s Name not found in localStorage.');
-        }
-
-
-        // Get all label tags that have the attribute 'for' and its value starts with 'answer'
-        const labels = document.querySelectorAll('label[for^="answer"]');
-
-        // Add click event listener to each label
-        labels.forEach(function (label) {
-            console.log(label);
-            label.addEventListener('click', function () {
-                // Get the label's text content
-                const text_to_input = label.textContent;
-                const survey_title = label.parentElement.parentElement.childNodes[0].textContent;
-
-                // Check if the input field exists and fill it up with a delay of 0.3 seconds
-                var counter = 0;
-                var intervalId = setInterval(function () {
-                    if(survey_title.includes("finish the final delivery")){
-                        var inputField = document.querySelector('[name="accept_or_deny"]');
-                        if (inputField) {
-                            changeValue(inputField, text_to_input);
-                        }
-                    }
-                    
-                    if(survey_title.includes("approve this edit")){
-                        inputField = document.querySelector('[name="revision_or_approval"]');
-                        if (inputField) {
-                            changeValue(inputField, text_to_input);
-                        }
-                    }
-                    
-                    if(survey_title.includes("subtitles attached")){
-                        inputField = document.querySelector('[name="subtitles"]');
-                        if (inputField) {
-                            changeValue(inputField, text_to_input);
-                        }
-                    }
-                    
-                    if(survey_title.includes("project types")){
-                        inputField = document.querySelector('[name="proj_type"]');
-                        if (inputField) {
-                            changeValue(inputField, text_to_input);
-                        }
-                    }
-
-                    counter++;
-                    if (counter == 3) {
-                        clearInterval(intervalId);
-                    }
-                    
-                }, 500);
-                // Replace 'Some text' with the desired value
-
-            });
-        });
-
-        // If there is a date field, it should be autofilled
-        function getCurrentFormattedDate(format) {
-            const currentDate = new Date();
-            const day = currentDate.getDate().toString().padStart(2, '0');
-            const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-            const year = currentDate.getFullYear().toString();
-
-            return `${month}/${day}/${year}`;
-
-        }
-
-
-        const dateInput = document.querySelector('input[name="date"]');
-        if (dateInput) {
-            const placeholderFormat = dateInput.getAttribute('placeholder');
-            const formattedDate = getCurrentFormattedDate(placeholderFormat);
-            // dateInput.value = formattedDate;
+        // If a value is found in the URL parameters, set it as the input's value
+        if (value) {
+            sleep_time += 100;
             setTimeout(function () {
-                changeValue(dateInput, formattedDate);
-            }, 1000);
-            // changeValue(dateInput, formattedDate);
+                changeValue(input, value.replaceAll("+", " "));
+            }, sleep_time);
         }
     });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Call the function to populate input fields on page load
+    setTimeout(populateInputFields, 1000);
+    // populateInputFields();
+
+    // Retrieve the 'editors_name' value from localStorage
+    var editorsName = localStorage.getItem('editors_name');
+    var editorsNameInput = document.querySelector('[name="editors_name"]');
+
+    // Check if the value exists in localStorage
+    if (editorsName) {
+        if (editorsNameInput) {
+            // Autofill input fields with the 'editors_name' attribute value
+            // editorsNameInput.value = editorsName;
+            setTimeout(function () {
+                changeValue(editorsNameInput, editorsName);
+            }, 1000);
+
+            // Add an input event listener to the input field
+            editorsNameInput.addEventListener('input', function () {
+                // Get the updated value from the input field
+                const newValue = editorsNameInput.value;
+
+                // Save the updated value to localStorage
+                localStorage.setItem('editors_name', newValue);
+
+                // Optionally, you can also display a confirmation message
+                console.log('Data saved to localStorage:', newValue);
+            });
+        }
+    } else {
+        // Handle the case where 'editors_name' is not found in localStorage
+        console.log('Editor\'s Name not found in localStorage.');
+    }
+
+    // Retrieve the 'email' value from localStorage
+    var email = localStorage.getItem('email');
+    var emailInput = document.querySelector('[name="email"]');
+
+    // Check if the value exists in localStorage
+    if (email) {
+        if (emailInput) {
+            // Autofill input fields with the 'editors_name' attribute value
+            // emailInput.value = email;
+            setTimeout(function () {
+                changeValue(emailInput, email);
+            }, 1000);
+
+            // Add an input event listener to the input field
+            emailInput.addEventListener('input', function () {
+                // Get the updated value from the input field
+                const newValue = emailInput.value;
+
+                // Save the updated value to localStorage
+                localStorage.setItem('email', newValue);
+
+                // Optionally, you can also display a confirmation message
+                console.log('Data saved to localStorage:', newValue);
+            });
+        }
+    } else {
+        // Handle the case where 'email' is not found in localStorage
+        console.log('Email not found in localStorage.');
+    }
+
+
+    // Get all label tags that have the attribute 'for' and its value starts with 'answer'
+    const labels = document.querySelectorAll('label[for^="answer"]');
+
+    // Add click event listener to each label
+    labels.forEach(function (label) {
+        // Get the label's text content
+        const text_to_input = label.textContent;
+        const survey_title = label.parentElement.parentElement.childNodes[0].textContent;
+        
+        label.addEventListener('click', function () {
+            // Check if the input field exists and fill it up with a delay of 0.3 seconds
+            var counter = 0;
+            var intervalId = setInterval(function () {
+                if (survey_title.includes("finish the final delivery")) {
+                    var inputField = document.querySelector('[name="accept_or_deny"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.includes("approve this edit")) {
+                    inputField = document.querySelector('[name="revision_or_approval"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.includes("subtitles attached")) {
+                    inputField = document.querySelector('[name="subtitles"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.includes("project types")) {
+                    inputField = document.querySelector('[name="proj_type"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("videos to be sifted")) {
+                    inputField = document.querySelector('[name="sifting"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("style of title animation")) {
+                    inputField = document.querySelector('[name="title_animations"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("where do you prefer your title animations to show")) {
+                    inputField = document.querySelector('[name="title_animations_2"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("which transition")) {
+                    inputField = document.querySelector('[name="transitions"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("and animations where concepts")) {
+                    inputField = document.querySelector('[name="graphics_and_animations"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("which font")) {
+                    inputField = document.querySelector('[name="fonts"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("short form captions")) {
+                    inputField = document.querySelector('[name="short_form_captions"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                if (survey_title.toLowerCase().includes("short form captions")) {
+                    inputField = document.querySelector('[name="short_form_captions"]');
+                    if (inputField) {
+                        changeValue(inputField, text_to_input);
+                    }
+                }
+
+                inputField = document.querySelector('[name="email"]');
+                if (inputField) {
+                    changeValue(inputField, localStorage.getItem("email") || "");
+                }
+
+                counter++;
+                if (counter == 3) {
+                    clearInterval(intervalId);
+                }
+
+            }, 500);
+            // Replace 'Some text' with the desired value
+
+        });
+    });
+
+    // If there is a date field, it should be autofilled
+    function getCurrentFormattedDate(format) {
+        const currentDate = new Date();
+        const day = currentDate.getDate().toString().padStart(2, '0');
+        const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+        const year = currentDate.getFullYear().toString();
+
+        return `${month}/${day}/${year}`;
+
+    }
+
+
+    const dateInput = document.querySelector('input[name="date"]');
+    if (dateInput) {
+        const placeholderFormat = dateInput.getAttribute('placeholder');
+        const formattedDate = getCurrentFormattedDate(placeholderFormat);
+        // dateInput.value = formattedDate;
+        setTimeout(function () {
+            changeValue(dateInput, formattedDate);
+        }, 1000);
+        // changeValue(dateInput, formattedDate);
+    }
+});
