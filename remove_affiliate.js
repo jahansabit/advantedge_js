2b3ca233304e24c1d1834cba2ceec8d845e25800
jahansabit <95720320+jahansabit@users.noreply.github.com>
2023-09-26 19:22:46 +0600

    added remove_affiliate.js

---
+++ remove_affiliate.js
@@ -0,0 +1,9 @@
+var affiliate_interval = setInterval(function(){
+    try {
+        document.querySelector('div.AffiliateBadgeUi-wzepun-0').remove();
+        console.log("removed");
+        clearInterval(affiliate_interval);
+    } catch(e) {
+        console.log("not removed");
+    }
+}, 100);
